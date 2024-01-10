18cc4cf121ed1a6c7ca31f652538c0750dc1cdbf
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-01-10 11:28:22 +0100

    design all product models

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 PRODUCT_FLAG = {
@@ -16,39 +18,34 @@ class Product(models.Model):
     flag = models.CharField(_('flag'),max_length=10,choices=PRODUCT_FLAG)
     price = models.FloatField(_('price'))
     sku = models.IntegerField(_('sku'))
-    brand = models.ForeignKey('brand',verbose_name=_('brand'),related_name='product_brand',on_delete=models.CASCADE)
+    brand = models.ForeignKey('Brand',verbose_name=_('brand'),related_name='product_brand',on_delete=models.CASCADE)
     tags = TaggableManager()
     subtitle = models.TextField(_('sbtitle'),max_length=500)
     describtion = models.TextField(_('describtion'),max_length=20000)
+    slug =models.SlugField(null=True,blank=True)
     def __str__(self):
         return self.name
 
 class Produkt_images(models.Model):
-    pass
-    '''
-    product
-    image
-    
-
-    '''
+    product = models.ForeignKey(Product,verbose_name= _('product'), related_name='product_image', on_delete=models.CASCADE)
+    image =models.ImageField(_('image'),uplode_to='product_image')
+    def __str__ (self):
+        return str(self.product)
 
 class Brand(models.Model):
-    
-    '''
-    name
-    image
-
-    '''
+    name = models.CharField(_('name'), max_length=100)
+    image = image =models.ImageField(_('image'),uplode_to='brand')
+    def __str__ (self):
+        return self.name
 
 
 
 class Reviews(models.Model):
-    pass
-
-    '''
-    user
-    product
-    review
-    date
-    rate
-    '''
+    user = models.ForeignKey(User,verbose_name=_('user'),related_name='review_author',on_delete=models.SET_NULL, null=True,blank=True)
+    product = models.ForeignKey(Product,verbose_name= _('product'), related_name='product_review', on_delete=models.CASCADE)
+    comment = models.CharField(_('comment'),max_length=200)
+    rate = models.IntegerField(-('rate'))
+    created_at = models.DateTimeField(default=timezone.now)
+    def __str__ (self):
+        return str(self.product)
+    
