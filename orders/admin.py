bb04550335876583b7fbf8a729ce443652bd1c9f
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-01-25 21:49:27 +0100

    add admin customization on orders, products

--- orders/admin.py
+++ orders/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Order,OrderDetail,Cart,CartDetail
 
-admin.site.register(Order)
-admin.site.register(OrderDetail)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_code','order_status','order_date','delivery_date']
+    list_filter = ['order_code','order_status']
+
+
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ['order','product','price','total']
+    list_filter = ['price','quantity']
+
+admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderDetail,OrderDetailAdmin)
 admin.site.register(Cart)
 admin.site.register(CartDetail)
 
