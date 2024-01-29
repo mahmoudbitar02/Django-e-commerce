a297f0810945679402c69949b4b8ea67a43ed44c
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-01-29 20:30:07 +0100

    add brand list with annotate

--- product/urls.py
+++ product/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 app_name = 'product'
 
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail,BrandList,BrandDetail
 
 urlpatterns = [
     path('', ProductList.as_view(),name='product_list'),
-    path('<slug:slug>', ProductDetail.as_view(),name='product_detail')
+    path('<slug:slug>', ProductDetail.as_view(),name='product_detail'),
+    path('brands/', BrandList.as_view(),name='brand_list'),
+    path('brands/<slug:slug>',BrandDetail.as_view(),name='brand_detail')
 ]
 
 
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Count
 from .models import Product,Produkt_images,Brand,Reviews
 
 # Create your views here.
@@ -11,3 +12,13 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product
+
+
+class BrandList(ListView):
+    model= Brand
+    queryset = Brand.objects.all().annotate(product_count=Count('product_brand'))
+
+
+
+class BrandDetail(DetailView):
+    model = Brand
