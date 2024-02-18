8bf65990d9c100bd9eaf11ffc38faad79082bb8e
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-18 17:41:51 +0100

    show product images in detail api

--- product/api.py
+++ product/api.py
@@ -2,25 +2,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
-from .serializers import ProductSerializer,BrandDetailSerializer,BrandListSerializer
+from .serializers import ProductDetailSerializer,ProductListSerializer,BrandDetailSerializer,BrandListSerializer
 from .models import Product,Brand
 
 @api_view(['GET'])
 def productlist_api(request):
     products=Product.objects.all()
-    data=ProductSerializer(products,many=True, context={'request':request}).data
+    data=ProductListSerializer(products,many=True, context={'request':request}).data
     return Response({'data':data})
 
 
 
 class ProductListApi(generics.ListCreateAPIView):
     queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+    serializer_class=ProductListSerializer
 
 
 class ProductDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+    serializer_class=ProductDetailSerializer
     lookup_field='slug'
 
 
--- product/serializers.py
+++ product/serializers.py
@@ -1,9 +1,14 @@
 # form
 from rest_framework import serializers
-from .models import Product,Brand
+from .models import Product,Brand,Produkt_images
+
+class ProduktImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Produkt_images
+        fields=['image']
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     #brand= BrandSerializer()  # damit alle Brand-fields auf der api seite ersehtlich sind 
     brand = serializers.StringRelatedField() # hier wird nur der name des Brandes gezeigt 
     price_with_tax = serializers.SerializerMethodField()
@@ -20,6 +25,17 @@ class ProductSerializer(serializers.ModelSerializer):
      #   return product.price*1.1
 
 
+class ProductDetailSerializer(serializers.ModelSerializer):
+    images= ProduktImagesSerializer(source='product_image',many=True)
+    brand = serializers.StringRelatedField() 
+
+
+    class Meta: 
+        model=Product
+        fields='__all__'
+
+ 
+
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
         model=Brand
@@ -28,7 +44,7 @@ class BrandListSerializer(serializers.ModelSerializer):
 
 
 class BrandDetailSerializer(serializers.ModelSerializer):
-    products=ProductSerializer(source='product_brand',many=True)
+    products=ProductListSerializer(source='product_brand',many=True)
     class Meta:
         model=Brand
         fields='__all__'
