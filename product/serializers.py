260feefd62a905c8bb590e9da74c51e43122b1c0
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-18 16:58:06 +0100

    add extra field with 2 ways

--- product/serializers.py
+++ product/serializers.py
@@ -13,6 +13,15 @@ class BrandSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     #brand= BrandSerializer()  # damit alle Brand-fields auf der api seite ersehtlich sind 
     brand = serializers.StringRelatedField() # hier wird nur der name des Brandes gezeigt 
+    #price_with_tax = serializers.SerializerMethodField()
+    price_with_tax = serializers.SerializerMethodField(method_name='myfunc')
+
     class Meta: 
         model=Product
         fields='__all__'
+
+    def get_price_with_tax(self,product):
+        return product.price*1.1
+
+    def myfunc(self,product):
+        return product.price*1.1
