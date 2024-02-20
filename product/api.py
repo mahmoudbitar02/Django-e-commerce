44546773876abcd7faa880237e8ee55fdda401d6
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-20 19:40:09 +0100

    add custom filter for product

--- product/api.py
+++ product/api.py
@@ -5,7 +5,9 @@ from rest_framework import generics
 from .serializers import ProductDetailSerializer,ProductListSerializer,BrandDetailSerializer,BrandListSerializer
 from .models import Product,Brand
 from .pagination import MyPagination
-import django_filters.rest_framework
+from .myfilter import ProductFilter
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 
@@ -21,8 +23,9 @@ class ProductListApi(generics.ListCreateAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductListSerializer
     pagination_class=MyPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_fields=['name','brand','price','flag']
+    filter_backends = [DjangoFilterBackend] 
+    #filterset_fields=['name','brand','price','flag']
+    filterset_class=ProductFilter
 
 class ProductDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset=Product.objects.all()
