62fb652c7aee95c5693e1e45330f668fe20fd6c1
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-22 18:57:01 +0100

    addd cart detail api

--- orders/api.py
+++ orders/api.py
@@ -7,12 +7,9 @@ from .serializers import CartSerializer
 from django.contrib.auth.models import User
 
 class CartDetailCreateApi(generics.GenericAPIView): # GenericAPIView damit wir die List,Detail,Crete bearbeiten können override
-    serializer_class=CartSerializer
-    queryset=Cart.objects.all()
-
-
-    def get(self, request,*args, **kwargs):
+    def get(self,request,*args,**kwargs):
         user_name=self.kwargs['username']
-        user=User.objects(username=user_name)
-        cart = Cart.objects.get(user=user,cart_status='Inprogress' )
-        return Cart
+        user = User.objects.get(username=user_name)
+        cart=Cart.objects.get(user=user,cart_status='Inprogress')
+        data=CartSerializer(cart).data
+        return Response({'cart':data})
