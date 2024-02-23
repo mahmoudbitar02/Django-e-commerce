2ca42bc527757450a2ab2290adf3309ea7e72116
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-23 16:49:38 +0100

    add show product in cart

--- orders/api.py
+++ orders/api.py
@@ -4,11 +4,11 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Order, OrderDetail, Cart,CartDetail
 from product.models import Product
-from .serializers import CartSerializer
+from .serializers import CartSerializer, CartDetailSerializer
 from django.contrib.auth.models import User
 
 class CartDetailCreateApi(generics.GenericAPIView): # GenericAPIView damit wir die List,Detail,Crete bearbeiten können override
-    serializer_class=CartSerializer
+    serializer_class=CartDetailSerializer
     def get(self,request,*args,**kwargs):
         user = User.objects.get(username=self.kwargs['username'])
         cart, created=Cart.objects.get_or_create(user=user,cart_status='Inprogress')
@@ -38,3 +38,7 @@ class CartDetailCreateApi(generics.GenericAPIView): # GenericAPIView damit wir d
         cart_data=CartDetail.objects.get(cart=cart ,product=product)
         cart_data.delete()
         return Response ({'status':200, 'Messege':'deleted successfully'})
+    
+
+
+
--- orders/serializers.py
+++ orders/serializers.py
@@ -2,7 +2,15 @@
 from rest_framework import serializers
 from .models import Order, OrderDetail, Cart,CartDetail
 
+
+class CartDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CartDetail
+        fields=['id','price','product','quantity','total']
+
+
 class CartSerializer(serializers.ModelSerializer):
+    cart_detail=CartDetailSerializer(many=True)#wenn cart_detail = der name vom dem relation muss hier kein Source hinzugefügt (source=cart_detail,many=True)
     class Meta:
         model=Cart
         fields='__all__'
