e09092e9388ab29779020ec65dea723dbbcb153d
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-02-28 20:14:42 +0100

    add translation

--- project/settings.py
+++ project/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-   # "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ar'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -153,10 +153,10 @@ INTERNAL_IPS = [
     # ...
 ]
 
-#LANGUAGES = [
- #   ("de", ("German")),
-  #  ("en", ("English")),
-#]
+LANGUAGES = [
+    ("ar", ("Arabic")),
+    ("en", ("English")),
+]
 
 
-LOCALE_PATH = ['locale']
+LOCALE_PATHS = ['locale']
