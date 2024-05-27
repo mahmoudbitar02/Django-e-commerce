54ba12c96c3afb0c1c7d80f5fb7694232b34f31d
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-05-27 19:09:06 +0200

    add user views basic structur, add user forms

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import signup, profile
+from .views import signup, profile, dashboard, activate_account
 app_name='accounts'
 
 urlpatterns = [
     path('profile/',profile, name = 'profile'),
     path('singup/',signup, name = 'signup'),
+    path('<str:username>/activate',activate_account, name = 'activate_account'),
+    path('dashboard',dashboard, name = 'dashboard'),
+
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,16 @@ from .models import Profile, Address, ContactNumbers
 def signup(request):
     pass
 
+def activate_account(requesst):
+    pass
+
+
 def profile(request):
     profile = Profile.objects.get(user=request.user)
     address = Address.objects.filter(user=request.user)
     contact_numbers = ContactNumbers.objects.filter(user=request.user)
     return render(request,'accounts/profile.html',{'profile':profile,'address':address,'contact_numbers':contact_numbers})
+
+
+def dashboard(request):
+    pass
